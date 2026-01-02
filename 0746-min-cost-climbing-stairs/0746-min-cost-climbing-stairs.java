fec69ec229ee04e5e502a7b7ad0de22fefb39d07
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-02 22:43:01 +0530

    Time: 0 ms (100%), Space: 45.1 MB (11.77%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp[] = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2 ; i<n;i++){
+            dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
+
+        // return Math.min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    }
+    // public int solve(int[] cost,int n,int[] dp){
+    //     if(n==1) return cost[1];
+    //     if(n==0) return cost[0];
+
+    //     if(dp[n]!=0)return dp[n];
+
+    //     dp[n] = cost[n] + Math.min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    //     return dp[n];
+    // }
+}
