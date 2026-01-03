920c026d92918e06adc9d6453b17a912b5249990
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-03 23:41:36 +0530

    Time: 1 ms (99.87%), Space: 46.9 MB (64.87%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int x : nums){
-            if(!set.contains(x)) set.add(x);
-            else{set.remove(x);}
+        int xor = 0;
+        for(int x:nums){
+            xor = xor^x;
         }
-        return set.iterator().next();
+        return xor;
+        
+        // Set<Integer> set = new HashSet<>();
+        // for(int x : nums){
+        //     if(!set.contains(x)) set.add(x);
+        //     else{set.remove(x);}
+        // }
+        // return set.iterator().next();
     }
 }
