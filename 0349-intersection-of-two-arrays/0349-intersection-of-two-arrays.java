53c111b7b3d03f6bde0a9da12cc4e5be2560cc88
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-04 19:23:40 +0530

    Time: 1 ms (98.94%), Space: 44.7 MB (86.21%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,19 +1,41 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> sans = new HashSet<>();
-        for(int x:nums1){
-            set1.add(x);
+        
+        boolean seen[] = new boolean[1001];
+        boolean added[] = new boolean[1001];
+        int count=0;
+        
+        for(int x:nums1) seen[x]=true;
+        for(int x:nums2) {
+            if(seen[x] && !added[x]){
+                added[x] = true;
+                count++;
+            }
         }
-        for(int x:nums2){
-            if(set1.contains(x)) sans.add(x);
+        int arr[] = new int[count];
+        int i = 0;
+        for(int j = 0;j<added.length;j++){
+            if(added[j]) {arr[i]=j; i++;}
         }
-        int arr[] = new int[sans.size()];
-        int i =0;
-        for(int x : sans){
-            arr[i] = x;
-            i++;
-        }
+
         return arr;
+        
+        
+        
+        // Set<Integer> set1 = new HashSet<>();
+        // Set<Integer> sans = new HashSet<>();
+        // for(int x:nums1){
+        //     set1.add(x);
+        // }
+        // for(int x:nums2){
+        //     if(set1.contains(x)) sans.add(x);
+        // }
+        // int arr[] = new int[sans.size()];
+        // int i =0;
+        // for(int x : sans){
+        //     arr[i] = x;
+        //     i++;
+        // }
+        // return arr;
     }
 }
