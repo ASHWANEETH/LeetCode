202532424aa60cd8e97073543f6615fe3e3c674f
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-04 22:28:38 +0530

    Time: 0 ms (100%), Space: 43.6 MB (40.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,46 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0;
-        int mid = 0;
-        int high = nums.length-1;
+        int low=0,mid=0,high = nums.length-1;
 
         while(mid<=high){
-            if(nums[mid]==0){
-                int temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
+            if(nums[mid]==1)mid++;
+            else if(nums[mid]==0){
+                swap(nums,mid,low);
                 low++;mid++;
             }
-            else if(nums[mid]==1)mid++;
             else if(nums[mid]==2){
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
+                swap(nums,mid,high);
                 high--;
             }
         }
-
-
-
-
-        // int z = 0;
-        // int o = 0;
-        // int t = 0;
-        // for(int x :nums){
-        //     switch(x){
-        //         case 0 : z++; break;
-        //         case 1 : o++; break;
-        //         case 2 : t++; break;
-        //     }
-        // }
-        // for(int i = 0;i<z;i++){
-        //     nums[i]=0;
-        // }
-        // for(int i = z;i<z+o;i++){
-        //     nums[i]=1;
-        // }
-        // for(int i = z+o;i<z+o+t;i++){
-        //     nums[i]=2;
-        // }
+    }
+    public void swap(int[] arr,int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
