addcb6ce1b4f73d4f4c15f4b96878b577608bd76
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-04 18:45:00 +0530

    Time: 6 ms (89.83%), Space: 57.9 MB (50.11%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -2,12 +2,27 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
 
         List<Integer> ans = new ArrayList<>();
-        for(int i = 0;i<nums.length;i++){
-            int idx = Math.abs(nums[i])-1;
 
-            if(nums[idx] < 0) ans.add(idx+1);
-            else nums[idx] = -nums[idx];
+        int i = 0;
+        while(i<nums.length){
+            int cr = nums[i]-1;
+            if(nums[i] != nums[cr]){
+                int temp = nums[i];
+                nums[i] = nums[cr];
+                nums[cr] = temp;
+            }else i++;
         }
+
+        for(i = 0 ; i< nums.length;i++){
+            if(nums[i]!=i+1) ans.add(nums[i]);
+        }
+
+        // for(int i = 0;i<nums.length;i++){
+        //     int idx = Math.abs(nums[i])-1;
+
+        //     if(nums[idx] < 0) ans.add(idx+1);
+        //     else nums[idx] = -nums[idx];
+        // }
 
         // Set<Integer> set = new HashSet<>();
         // for(int x:nums){
