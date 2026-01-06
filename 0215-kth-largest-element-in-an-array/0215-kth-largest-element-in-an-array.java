168a458874f57ef788d5d83767e3b3ba8f41502a
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-06 08:06:22 +0530

    Time: 2236 ms (5%), Space: 70.6 MB (7.85%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,14 +1,45 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int freq[] = new int[20001];
-        for(int x:nums){
-            freq[10000+x]++;
+        
+        //If range is small and given
+        // int freq[] = new int[20001];
+        // for(int x:nums){
+        //     freq[10000+x]++;
+        // }
+        // int i = freq.length-1;
+        // while(k!=0){
+        //     if(freq[i]==0){i--;continue;}
+        //     else{freq[i]--;k--;}
+        // }
+        // return i-10000;
+
+
+        int l = 0;
+        int r = nums.length-1;
+
+        while(true){
+            int p = part(nums,l,r);
+            if(p == k-1) return nums[p];
+            else if(p<k-1) {l=p+1;}
+            else r=p-1;
         }
-        int i = freq.length-1;
-        while(k!=0){
-            if(freq[i]==0){i--;continue;}
-            else{freq[i]--;k--;}
+
+    }
+    public int part(int[] arr,int l,int r){
+        int p = arr[r];
+        int i = l;
+
+        for(int j = l;j<r;j++){
+            if(arr[j]>p){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++; 
+            }
         }
-        return i-10000;
+        int temp = arr[i];
+        arr[i] = arr[r];
+        arr[r] = temp;
+        return i;
     }
 }
