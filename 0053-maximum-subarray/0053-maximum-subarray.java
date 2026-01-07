6cfc1db5957edcc880f8d1a97328410f8e4b67ef
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-07 11:49:44 +0530

    Time: 1 ms (99.72%), Space: 77.3 MB (32.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,9 +3,9 @@ class Solution {
         int total = 0;
         int res = nums[0];
         for(int i = 0;i<nums.length;i++){
-            if(total<0)total=0;
             total += nums[i];
             res = Math.max(res,total);
+            if(total<0)total=0;
         }
         return res;
     }
