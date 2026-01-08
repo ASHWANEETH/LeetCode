d479d2b9a797e88562b99d56905e777fc1606151
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-08 10:49:24 +0530

    Time: 0 ms (100%), Space: 43 MB (47.85%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean check(int[] nums) {
+    
+    //If more then one i>i+1 -> array cant be rotated
         int c = 0;
         for(int i = 0;i<nums.length-1;i++){
             if(nums[i]>nums[i+1])c++;
