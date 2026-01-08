7da64703ca4a9d887c7cbbc4663984a84ff2f649
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-08 10:48:23 +0530

    Time: 0 ms (100%), Space: 43.3 MB (13.43%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,20 +1,28 @@
 class Solution {
     public boolean check(int[] nums) {
-        
-        int l = nums[nums.length-1];
-        int i = nums.length-1;
-        while(i>0 && nums[i]>=nums[i-1]){
-            i--;
+        int c = 0;
+        for(int i = 0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1])c++;
+            if(c>=2) return false;
         }
-        if(i==0)return true;
-        int j=0;
-        while(j<i){
-            if(nums[j]>=l){
-                l=nums[j];
-                j++;
-            }
-            else return false;
-        }
-        return true;
+        if(nums[nums.length-1]>nums[0])c++;
+        return c<2;
+
+    //-------// my solution :)
+    //     int l = nums[nums.length-1];
+    //     int i = nums.length-1;
+    //     while(i>0 && nums[i]>=nums[i-1]){
+    //         i--;
+    //     }
+    //     if(i==0)return true;
+    //     int j=0;
+    //     while(j<i){
+    //         if(nums[j]>=l){
+    //             l=nums[j];
+    //             j++;
+    //         }
+    //         else return false;
+    //     }
+    //     return true;
     }
 }
