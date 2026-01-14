c8e1238853dfc959d27d22dfaac6350f54f3fd63
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-14 18:34:36 +0530

    Time: 10 ms (24.75%), Space: 48.9 MB (83.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,21 +1,36 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<List<Integer>> l = new ArrayList<>();
         Arrays.sort(intervals, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
-        
-        for(int i =0;i<intervals.length;i++){
-            int s = intervals[i][0];
-            int e = intervals[i][1];
-            if(!l.isEmpty() && e <= l.get(l.size()-1).get(1))continue;
 
-            for(int j = i+1;j<intervals.length;j++){
-                if(e>=intervals[j][0]){
-                    e = Math.max(intervals[j][1],e);
-                }
-                else break;
+        List<List<Integer>> l = new ArrayList<>();
+        l.add(Arrays.asList(intervals[0][0],intervals[0][1]));
+
+        for(int i = 1;i<intervals.length;i++){
+            if(intervals[i][0] <= l.get(l.size()-1).get(1)){
+                l.get(l.size()-1).set(1,Math.max(l.get(l.size()-1).get(1),intervals[i][1]));
             }
-            l.add(Arrays.asList(s,e));
+            else{
+                l.add(Arrays.asList(intervals[i][0],intervals[i][1]));
+            }
         }
+        
+        
+        
+        
+        //-----------brute
+        // for(int i =0;i<intervals.length;i++){
+        //     int s = intervals[i][0];
+        //     int e = intervals[i][1];
+        //     if(!l.isEmpty() && e <= l.get(l.size()-1).get(1))continue;
+
+        //     for(int j = i+1;j<intervals.length;j++){
+        //         if(e>=intervals[j][0]){
+        //             e = Math.max(intervals[j][1],e);
+        //         }
+        //         else break;
+        //     }
+        //     l.add(Arrays.asList(s,e));
+        // }
         int[][] arr = new int[l.size()][2];
 
         for(int i = 0; i < l.size(); i++){
