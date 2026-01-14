37d35e0f41b77789d7c26e1cb207b090bfbc1e43
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-14 18:45:40 +0530

    Time: 8 ms (89.41%), Space: 49.1 MB (58.95%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,17 +2,22 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
 
-        List<List<Integer>> l = new ArrayList<>();
-        l.add(Arrays.asList(intervals[0][0],intervals[0][1]));
+        List<int[]> l = new ArrayList<>();
+        int s = intervals[0][0];
+        int e = intervals[0][1];
 
         for(int i = 1;i<intervals.length;i++){
-            if(intervals[i][0] <= l.get(l.size()-1).get(1)){
-                l.get(l.size()-1).set(1,Math.max(l.get(l.size()-1).get(1),intervals[i][1]));
+            if(e >= intervals[i][0]){
+                e = Math.max(e,intervals[i][1]);
             }
             else{
-                l.add(Arrays.asList(intervals[i][0],intervals[i][1]));
+                l.add(new int[]{s,e});
+                s = intervals[i][0];
+                e = intervals[i][1];
             }
         }
+        l.add(new int[]{s,e});
+        return l.toArray(new int[l.size()][2]);
         
         
         
@@ -31,12 +36,12 @@ class Solution {
         //     }
         //     l.add(Arrays.asList(s,e));
         // }
-        int[][] arr = new int[l.size()][2];
+        // int[][] arr = new int[l.size()][2];
 
-        for(int i = 0; i < l.size(); i++){
-            arr[i][0] = l.get(i).get(0);
-            arr[i][1] = l.get(i).get(1);
-        }
-        return arr;
+        // for(int i = 0; i < l.size(); i++){
+        //     arr[i][0] = l.get(i).get(0);
+        //     arr[i][1] = l.get(i).get(1);
+        // }
+        // return arr;
     }
 }
