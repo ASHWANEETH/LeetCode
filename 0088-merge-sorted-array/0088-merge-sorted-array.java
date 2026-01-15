9ce4b1e69221286d4ebebf4decf9501d4e1304ad
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-15 14:13:23 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,7 +5,16 @@ class Solution {
         int r = n-1;
 
         while(r>=0){
-            nums1[x--] = l>=0 && (nums1[l]>nums2[r]) ? nums1[l--] : nums2[r--];
+            if(l>=0 && nums1[l]>nums2[r]){
+                nums1[x] = nums1[l];
+                l--;
+            }
+            else{
+                nums1[x] = nums2[r];
+                r--;
+            }
+            x--;
+            // nums1[x--] = (l>=0 && (nums1[l]>nums2[r])) ? nums1[l--] : nums2[r--]; -> this also works
         }
         
         // for(int i = 0;i<nums2.length;i++){
