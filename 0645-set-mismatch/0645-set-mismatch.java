c5cbb57c0d58c7a1b0f510848814c149eeff5f3e
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-16 12:18:57 +0530

    Time: 3 ms (62.6%), Space: 47.6 MB (69.34%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -17,5 +17,18 @@ class Solution {
         m = xy - r;
 
         return new int[]{r,m};
+
+        
+        // int arr[] = new int[nums.length];
+        // int ans[] = new int[2];
+
+        // for(int x:nums){
+        //     arr[x-1]++;
+        // }
+        // for(int i = 0;i<arr.length;i++){
+        //     if(arr[i]==2)ans[0]=i+1;
+        //     if(arr[i]==0)ans[1]=i+1;
+        // }
+        // return ans;
     }
 }
