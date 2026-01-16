1747f0a7a07c7198af669914de7d6f6a90be169a
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-16 16:15:32 +0530

    Time: 0 ms (100%), Space: 44.3 MB (13.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,22 +2,36 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i = 0;i<m;i++){
-            if(matrix[i][0]<=target && target<=matrix[i][n-1]){
-              return bs(matrix[i],target);
-            }
+
+        int l = 0;
+        int h = m*n-1; 
+        while(l<=h){
+            int mid = l + ((h-l)/2);
+            int r = mid/n;
+            int c = mid%n;
+            if(matrix[r][c]==target)return true;
+            else if(matrix[r][c]>target)h = mid - 1;
+            else l = mid + 1;
         }
         return false;
+
+
+        // for(int i = 0;i<m;i++){
+        //     if(matrix[i][0]<=target && target<=matrix[i][n-1]){
+        //       return bs(matrix[i],target);
+        //     }
+        // }
+        // return false;
     }
-    public boolean bs(int arr[],int t){
-        int low = 0;
-        int high = arr.length-1;
-        while(low<=high){
-            int mid = low + ((high-low)/2);
-            if(arr[mid]==t)return true;
-            else if(arr[mid]>t) high = mid- 1;
-            else low = mid + 1;
-        }
-        return false;
-    }
+    // public boolean bs(int arr[],int t){
+    //     int low = 0;
+    //     int high = arr.length-1;
+    //     while(low<=high){
+    //         int mid = low + ((high-low)/2);
+    //         if(arr[mid]==t)return true;
+    //         else if(arr[mid]>t) high = mid- 1;
+    //         else low = mid + 1;
+    //     }
+    //     return false;
+    // }
 }
