56865c772ed39d356415db0718a76263db87e892
ASHWANEETH H SIDDANNAVAR <97220582+ASHWANEETH@users.noreply.github.com>
2026-01-17 20:16:26 +0530

    Time: 0 ms (100%), Space: 45.9 MB (16.47%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,16 +15,23 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        boolean d[] = new boolean[]{true};
-        helper(root,d);
-        return d[0];
+        return helper(root) != -1;
+        // boolean d[] = new boolean[]{true};
+        // helper(root,d);
+        // return d[0];
     }
-    public int helper(TreeNode node,boolean[] d){
+    public int helper(TreeNode node){
         if(node==null)return 0;
-        if(!d[0])return 0;
-        int lh = helper(node.left,d);
-        int rh = helper(node.right,d);
-        if(Math.max(lh,rh)-Math.min(lh,rh)>1)d[0]= false;
+        // if(!d[0])return 0;
+
+        int lh = helper(node.left);
+        if(lh == -1)return -1;
+
+        int rh = helper(node.right);
+        if(rh == -1)return -1;
+
+        if(Math.abs(lh-rh)>1)return -1;
+
         return 1+Math.max(lh,rh);
     }
 }
