ee7455868bb750208c6c7418b942af6d979f8d20
ASHWANEETH H SIDDANNAVAR <ashwaneeth@gmail.com>
2026-08-26 14:06:40 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.43%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;        // make space for next bit
+            ans |= (n & 1);   // take last bit of n
+            n >>= 1;          // remove last bit
+        }
+
+        return ans;
+    }
+}
